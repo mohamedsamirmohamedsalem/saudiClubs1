edec8ecf756ec2c46a78959ed9dbb98824d50790
Mohamed Samir <mohamedsamirsalem100@gmail.com>
2019-11-27 11:28:18 +0200

    design for select payment view controller has been added successfully

---
+++ saudiClubs1/Controller/ResetPasswordViewController3.swift
@@ -0,0 +1,30 @@
+//
+//  ResetPasswordViewController3.swift
+//  saudiClubs1
+//
+//  Created by Mohamed Samir on 11/27/19.
+//  Copyright © 2019 Mohamed Samir. All rights reserved.
+//
+
+import UIKit
+
+class ResetPasswordViewController3: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ saudiClubs1/Controller/selectPaymentViewController.swift
@@ -0,0 +1,21 @@
+//
+//  selectPaymentViewController.swift
+//  saudiClubs1
+//
+//  Created by Mohamed Samir on 11/27/19.
+//  Copyright © 2019 Mohamed Samir. All rights reserved.
+//
+
+import UIKit
+
+class selectPaymentViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
